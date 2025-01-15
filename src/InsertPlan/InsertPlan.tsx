d94652696f07ed69cfd90e1d991fac6868b98e03
SangHwi-Back <sanghwiback@gmail.com>
2025-01-15 14:40:34 +0900

    Add Insert page

---
+++ src/InsertPlan/InsertPlan.tsx
@@ -0,0 +1,57 @@
+import mainStyle from '../main.module.css';
+import shrinkStyle from '../shrink.module.css';
+import styles from './insertPlan.module.css';
+import {useState} from "react";
+import {PlanTag} from "../DailyPlanerContainer/DailyPlanerContainer.tsx";
+import minus from '../assets/minus.png';
+import plus from '../assets/plus.png';
+
+export default function InsertPlan() {
+  const [title, setTitle] = useState<string>('');
+  const [tags, setTags] = useState<PlanTag[]>([]);
+  
+  return (
+    <div className={mainStyle.roundedRectangle + shrinkStyle.shrink}>
+      <div className={styles.background}>
+        
+        <div className={styles.titleArea}>플랜 추가하기</div>
+        
+        <div className={styles.subTitleArea}>제목</div>
+        
+        <input type={'text'}
+               value={title}
+               onChange={(e) => setTitle(e.target.value)}
+               className={styles.titleTextField}/>
+        
+        <div className={styles.subTitleArea}>
+          태그 추가
+          <img src={plus}
+               onClick={() => setTags([{name: ''}, ...tags])}
+               alt={'plus'}
+               className={styles.plusMinusButton}/>
+        </div>
+        
+        <div className={styles.tagArea}>
+          {tags.map((tag, index) => (
+            <div className={styles.tagElements} key={index}>
+              <img src={minus}
+                   onClick={() => setTags((prev) => {
+                     prev.slice(index, 1);
+                     return prev;
+                   })}
+                   alt={'minus'}
+                   className={styles.plusMinusButton}/>
+              <input type={'text'} value={tag.name} onChange={(e) => {
+                setTags((prev) => {
+                  return prev.map((_tag, _index) =>
+                    _index === index ? {...tag, name: e.target.value} : _tag
+                  );
+                });
+              }}/>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App.tsx'
 import PlanContextProvider from "./context/PlanContextProvider.tsx";
 import ModalContextProvider from "./context/ModalContextProvider.tsx";
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import InsertPlan from "./InsertPlan/InsertPlan.tsx";
 
 const root = document.getElementById('root') as HTMLElement;
 root.style.backgroundColor = '#FFFFFF';
@@ -15,8 +17,13 @@ createRoot(root).render(
   <StrictMode>
     <ModalContextProvider>
       <PlanContextProvider>
-        <App />
+        <BrowserRouter>
+          <Routes>
+            <Route path={'/'} element={<App/>}/>
+            <Route path={'/insert'} element={<InsertPlan/>}/>
+          </Routes>
+        </BrowserRouter>
       </PlanContextProvider>
     </ModalContextProvider>
-  </StrictMode>,
+  </StrictMode>
 )
